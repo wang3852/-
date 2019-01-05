6372868b47fb3d4a9c465e34faf2ca022877a52f
unknown <1352584289@qq.com>
2019-01-05 20:05:56 +0800

    完成角色列表展开行

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -9,38 +9,48 @@
       </el-col>
     </el-row>
     <!-- 表格 -->
-    <el-table height="300px" :data="roles" style="width: 100%">
-      <el-table-column type="index" label="#" width="120"></el-table-column>
-      <el-table-column prop="roleDesc" label="角色名称" width="180"></el-table-column>
-      <el-table-column prop="roleName" label="角色描述" width="180"></el-table-column>
-    
-      <el-table-column prop label="操作" width="300">
+    <el-table type="expand" height="400px" :data="roles" style="width: 100%">
+      <el-table-column el-table-column type="expand" width="80">
+        <template slot-scope="scope">
+          <el-row v-for="(item1,i) in scope.row.children" :key="i">
+
+            <el-col :span=4>
+              <el-tag class="level1" closable type='success'>{{item1.authName}}</el-tag>
+               <i class="el-icon-arrow-right"></i>
+            </el-col>
+
+            <el-col :span=20>
+              <el-row  v-for="(item2,i) in item1.children" :key="i">
+                <el-col :span=4>
+                  <el-tag class="level2" closable type="info">{{item2.authName}}</el-tag>
+                    <i class="el-icon-arrow-right"></i>
+                </el-col>
+                 <el-col :span=20>    
+                    <el-tag class="level3" closable v-for="(item3,i) in item2.children" :key="i">{{item3.authName}}</el-tag>
+                 </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          
+          <!-- 没有权限 -->
+          <el-row v-if="scope.row.children.length===0">
+            <el-col>
+              <span>该角色没有任何权限</span>
+            </el-col>
+          </el-row>
+
+        </template>
+      </el-table-column>
+      <el-table-column type="index" label="#" width="180"></el-table-column>
+      <el-table-column prop="roleDesc" label="角色名称" width="240"></el-table-column>
+      <el-table-column prop="roleName" label="角色描述" width="240"></el-table-column>
+
+      <el-table-column prop label="操作" width="360">
         <template slot-scope="scope">
           <el-row>
-            <el-button
-              type="primary"
-              plain
-              size="mini"
-              icon="el-icon-edit"
-         
-              circle
-            ></el-button>
-            <el-button
-              type="danger"
-              plain
-              size="mini"
-              icon="el-icon-delete"
-           
-              circle
-            ></el-button>
-            <el-button
-              type="success"
-              plain
-              size="mini"
-              icon="el-icon-check"
-         
-              circle
-            ></el-button>
+            <el-button type="primary"  plain size="mini" icon="el-icon-edit" circle></el-button>
+            <el-button type="danger" plain size="mini" icon="el-icon-delete" circle></el-button>
+            <el-button type="success" plain size="mini" icon="el-icon-check" circle></el-button>
           </el-row>
         </template>
       </el-table-column>
@@ -65,8 +75,10 @@ export default {
       const token = localStorage.getItem("token");
       this.$http.defaults.headers.common["Authorization"] = token;
       const res = await this.$http.get("roles");
-      console.log(res);
+      // console.log(res);
       this.roles = res.data.data;
+      console.log(this.roles);
+      
     }
   }
 };
@@ -79,4 +91,11 @@ export default {
 .row {
   margin-top: 20px;
 }
+.level1,.level2,.level3{
+  margin-top:10px;
+}
+.level3 {
+  margin-left: 5px;
+}
+
 </style>
