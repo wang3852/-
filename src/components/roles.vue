bed320ab6647302dc21517a651071b2576467c6f
unknown <1352584289@qq.com>
2019-01-06 15:57:29 +0800

    统一配置请求头

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -10,35 +10,50 @@
     </el-row>
     <!-- 表格 -->
     <el-table type="expand" height="400px" :data="roles" style="width: 100%">
+      <!-- 展开行 -->
       <el-table-column el-table-column type="expand" width="80">
         <template slot-scope="scope">
+          <!-- 处理有权限 -->
           <el-row v-for="(item1,i) in scope.row.children" :key="i">
-
-            <el-col :span=4>
-              <el-tag class="level1" closable type='success'>{{item1.authName}}</el-tag>
-               <i class="el-icon-arrow-right"></i>
+            <el-col :span="4">
+              <el-tag
+                class="level1"
+                @close="deleRights(scope.row,item1)"
+                closable
+                type="success"
+              >{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
             </el-col>
-
-            <el-col :span=20>
-              <el-row  v-for="(item2,i) in item1.children" :key="i">
-                <el-col :span=4>
-                  <el-tag class="level2" closable type="info">{{item2.authName}}</el-tag>
-                    <i class="el-icon-arrow-right"></i>
+            <el-col :span="20">
+              <el-row v-for="(item2,i) in item1.children" :key="i">
+                <el-col :span="4">
+                  <el-tag
+                    class="level2"
+                    @close="deleRights(scope.row,item2)"
+                    closable
+                    type="info"
+                  >{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
                 </el-col>
-                 <el-col :span=20>    
-                    <el-tag class="level3" closable v-for="(item3,i) in item2.children" :key="i">{{item3.authName}}</el-tag>
-                 </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    class="level3"
+                    @close="deleRights(scope.row,item3)"
+                    closable
+                    v-for="(item3,i) in item2.children"
+                    :key="i"
+                  >{{item3.authName}}</el-tag>
+                </el-col>
               </el-row>
             </el-col>
           </el-row>
-          
+
           <!-- 没有权限 -->
           <el-row v-if="scope.row.children.length===0">
             <el-col>
               <span>该角色没有任何权限</span>
             </el-col>
           </el-row>
-
         </template>
       </el-table-column>
       <el-table-column type="index" label="#" width="180"></el-table-column>
@@ -48,13 +63,37 @@
       <el-table-column prop label="操作" width="360">
         <template slot-scope="scope">
           <el-row>
-            <el-button type="primary"  plain size="mini" icon="el-icon-edit" circle></el-button>
+            <el-button type="primary" plain size="mini" icon="el-icon-edit" circle></el-button>
             <el-button type="danger" plain size="mini" icon="el-icon-delete" circle></el-button>
-            <el-button type="success" plain size="mini" icon="el-icon-check" circle></el-button>
+            <el-button
+              type="success"
+              @click="showDiaRight(scope.row)"
+              plain
+              size="mini"
+              icon="el-icon-check"
+              circle
+            ></el-button>
           </el-row>
         </template>
       </el-table-column>
     </el-table>
+
+    <!-- 分配权限的对话框 -->
+    <el-dialog title="提示" :visible.sync="dialogVisibleRight" width="30%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisibleRight = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisibleRight = false">确 定</el-button>
+      </span>
+
+      <el-tree
+        :data="treeData"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+        :props="defaultProps"
+      ></el-tree>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -62,23 +101,59 @@
 export default {
   data() {
     return {
-      roles: []
+      roles: [],
+      treeData:[],
+      dialogVisibleRight: false,
+      defaultProps:{
+        label:"authName",
+        children:"children"
+      }
     };
   },
   created() {
     this.getRoles();
   },
   methods: {
+    // 展示分配权限对话框
+    async showDiaRight(right) {
+      // 获取权限树
+      const res = await this.$http.get('rights/tree');
+      console.log(res);
+      this.treeData = res.data.data;
+      console.log(this.treeData);
+      
+      this.dialogVisibleRight = true;
+
+    },
+    // 取消权限
+    async deleRights(role, right) {
+      // console.log(role);
+      // console.log(right);
+      // 发请求
+      // 角色id
+      // 权限id
+      // const token = localStorage.getItem("token");
+      // this.$http.defaults.headers.common["Authorization"] = token;
+     
+      
+      const res = await this.$http.delete(
+        `roles/${role.id}/rights/${right.id}`
+      );
+    
+      // 当前角色刷新
+      role.children = res.data.data;
+      // console.log(res);
+
+      // 全部刷新
+      // this.getRoles();
+    },
     // 获取角色列表数据
     async getRoles() {
       //  请求头
-      const token = localStorage.getItem("token");
-      this.$http.defaults.headers.common["Authorization"] = token;
       const res = await this.$http.get("roles");
-      // console.log(res);
+      console.log(res);
       this.roles = res.data.data;
-      console.log(this.roles);
-      
+    
     }
   }
 };
@@ -91,11 +166,12 @@ export default {
 .row {
   margin-top: 20px;
 }
-.level1,.level2,.level3{
-  margin-top:10px;
+.level1,
+.level2,
+.level3 {
+  margin-top: 10px;
 }
 .level3 {
   margin-left: 5px;
 }
-
 </style>
--- src/plugin/http.js
+++ src/plugin/http.js
@@ -1,12 +1,25 @@
-
 // 发送请求
 import axios from 'axios'
 const Http = {}
 Http.install = function (Vue) {
-  axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+	axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 
-  // 4. 添加实例方法
-  Vue.prototype.$http = axios
+	axios.interceptors.request.use(function (config) {
+		// 在发送请求之前做些什么
+		console.log(config);
+		
+		if (config.url !== 'login') {
+			const token = localStorage.getItem('token')
+			config.headers.Authorization = token;
+		}
+		return config
+	}, function (error) {
+		// 对请求错误做些什么
+		return Promise.reject(error)
+	})
+
+	// 4. 添加实例方法
+	Vue.prototype.$http = axios
 }
 //   导出对象
 export default Http
