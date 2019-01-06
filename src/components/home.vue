dae258e4c1c812d44857fb58ea496fc74d8c8de5
unknown <1352584289@qq.com>
2019-01-06 18:01:49 +0800

    动态导航菜单

--- src/components/home.vue
+++ src/components/home.vue
@@ -18,72 +18,17 @@
       <el-aside width="200px" class="aside">
         <el-menu router default-active="2" unique-opened>
             <!-- 1 -->
-          <el-submenu index="1">
+          <el-submenu :index="item1.order" v-for="(item1,i) in menu" :key="item1.id">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{item1.authName}}</span>
             </template>
-            <el-menu-item index="users">
+            <el-menu-item :index="item2.path" v-for="(item2,i) in item1.children" :key="item2.id">
               <i class="el-icon-menu"></i>
-              用户列表
+              {{item2.authName}}
             </el-menu-item>
           </el-submenu>
-          <!-- 2 -->
-           <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="roles">
-              <i class="el-icon-menu"></i>
-              角色列表
-            </el-menu-item>
-             <el-menu-item index="rights">
-              <i class="el-icon-menu"></i>
-              权限列表
-            </el-menu-item>
-          </el-submenu>
-          <!-- 3 -->
-           <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-            <el-menu-item index="3-1">
-              <i class="el-icon-menu"></i>
-              商品列表
-            </el-menu-item>
-            <el-menu-item index="3-2">
-              <i class="el-icon-menu"></i>
-              分类参数
-            </el-menu-item>
-            <el-menu-item index="3-3">
-              <i class="el-icon-menu"></i>
-              商品分类
-            </el-menu-item>
-          </el-submenu>
-          <!-- 4 -->
-           <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-            <el-menu-item index="4-1">
-              <i class="el-icon-menu"></i>
-              订单列表
-            </el-menu-item>
-          </el-submenu>
-          <!-- 5 -->
-           <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-            <el-menu-item index="5-1">
-              <i class="el-icon-menu"></i>
-              数据报表
-            </el-menu-item>
-          </el-submenu>
+        
         </el-menu>
       </el-aside>
 
@@ -96,6 +41,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      menu:[]
+    }
+  },
 // 在渲染组件前，判断token,否，提示登录，有，进入home
 beforeCreate() {
   if (!localStorage.getItem('token')) {
@@ -109,9 +59,22 @@ beforeCreate() {
     
   }
 },
+created() {
+this.getMenu();
+},
 
   // 退出
   methods:{
+    // 菜单导航
+   async getMenu() {
+      // 发请求
+      const res = await this.$http.get(`menus`);
+      // console.log(res);
+      this.menu = res.data.data;
+      console.log(this.menu);
+      
+      
+    },
     handlelogout() {
       // 回到登录页
       this.$router.push({
