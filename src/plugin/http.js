dae60584a235e07e193c5c5e284b1c1db8b3d49b
unknown <1352584289@qq.com>
2019-01-11 17:04:05 +0800

    处理拦截响应

--- src/plugin/http.js
+++ src/plugin/http.js
@@ -1,5 +1,6 @@
 // 发送请求
 import axios from 'axios'
+import { Message } from 'element-ui'
 const Http = {}
 Http.install = function (Vue) {
 	axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
@@ -16,7 +17,25 @@ Http.install = function (Vue) {
 	}, function (error) {
 		// 对请求错误做些什么
 		return Promise.reject(error)
-	})
+	});
+	// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+	// 对响应数据做点什么
+	console.log('拦截响应');
+	console.log(response);
+	const {meta:{msg,status}}=response;
+	// 拦截不是200 和201的请求吧
+	
+	if (status!==200 & status!==201) {
+		Message.waring(msg);
+		
+	}
+	
+	return response;
+}, function (error) {
+	// 对响应错误做点什么
+	return Promise.reject(error);
+});
 
 	// 4. 添加实例方法
 	Vue.prototype.$http = axios
