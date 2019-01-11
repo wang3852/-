969c14362ab885ce00890b52cf26147afe02387c
unknown <1352584289@qq.com>
2019-01-11 16:43:32 +0800

    完成数据报表

--- src/components/report.vue
+++ src/components/report.vue
@@ -4,47 +4,82 @@
     <my-bread level1="数据统计" level2="数据报表"></my-bread>
     <!-- 图表 -->
     <div id="main" style="width: 600px;height:400px;"></div>
-
   </el-card>
 </template>
 
 <script>
-import echarts from 'echarts'
+import echarts from "echarts";
 export default {
-  mounted () {
+  mounted() {
     this.initEcharts();
   },
-  methods:{
-    initEcharts() {
-       // 基于准备好的dom，初始化echarts实例
-        const myChart = echarts.init(document.getElementById('main'));
+  methods: {
+   async initEcharts() {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(document.getElementById("main"));
+      // 
 
-        // 指定图表的配置项和数据
-        const option = {
-            title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            legend: {
-                data:['销量']
-            },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        };
+      // 指定图表的配置项和数据
+      // const option1 = {
+      //   title: {
+      //     text: "ECharts 入门示例"
+      //   },
+      //   tooltip: {},
+      //   legend: {
+      //     data: ["销量"]
+      //   },
+      //   xAxis: {
+      //     data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      //   },
+      //   yAxis: {},
+      //   series: [
+      //     {
+      //       name: "销量",
+      //       type: "bar",
+      //       data: [5, 20, 36, 10, 10, 20]
+      //     }
+      //   ]
+      // };
+      const option1 = {
+        title: {
+         
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#6a7985"
+            }
+          }
+        },
+      
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true
+        }
+       
+       
+      };
+      // 发请求获取数据
+      const res = await this.$http.get(`reports/type/1`)
+      console.log(res);
 
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
+    const option2 = res.data.data;
+    const option = {...option1,...option2};
+  
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
     }
   }
-
-}
+};
 </script>
 
 <style>
